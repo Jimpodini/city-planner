f274c17748e710d5cad36beadeb2cd66727bbff9
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-01 07:57:30 +0200

    Use activity and modal component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'city-planner';
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    console.log(this.authService.authObject);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
