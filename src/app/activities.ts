514ed955769ac147bb3808d31e00321c63d278a1
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-02 20:55:21 +0200

    Add acitivities and images

--- src/app/activities.ts
+++ src/app/activities.ts
@@ -1,8 +1,20 @@
 export const ACTIVITIES = [
   {
     name: 'Vasaparken',
-    category: 'Park',
-    img: '',
+    category: 'Parks',
+    img: 'vasaparken.jpg',
     googlePlaceId: 'ChIJhxFeG3udX0YRmSWF9cSLKeQ',
+  },
+  {
+    name: 'Melanders',
+    category: 'Restaurants',
+    img: 'melanders.jpg',
+    googlePlaceId: 'ChIJgaLDAmWdX0YR5nM3muriMC4',
+  },
+  {
+    name: 'Boqueria',
+    category: 'Bars',
+    img: 'boqueria.jpg',
+    googlePlaceId: 'ChIJ5XkeUlydX0YRZI4qUQABRzU',
   },
 ];
