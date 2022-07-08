ebe965827e812a8cc181917c5c80b48bf5839944
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-08 19:33:44 +0200

    Get stay from firebase db

--- src/app/core/feature-area/feature-area.component.ts
+++ src/app/core/feature-area/feature-area.component.ts
@@ -6,7 +6,10 @@ import {
   collection,
   setDoc,
   doc,
+  getDoc,
+  DocumentData,
 } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth.service';
 
@@ -18,8 +21,13 @@ import { AuthService } from 'src/app/auth.service';
 export class FeatureAreaComponent implements OnInit {
   // item$: Observable<any[]>;
 
-  constructor(public authService: AuthService, firestore: Firestore) {
-    // const test = collection(firestore, 'stays');
+  constructor(
+    public authService: AuthService,
+    private firestore: Firestore,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.getStay();
     // this.item$ = collectionData(test);
     // this.item$.subscribe((value) => console.log(value));
     // setDoc(doc(test, 'Hhgph1QjkxcwCIWNJiAu'), {
@@ -27,7 +35,21 @@ export class FeatureAreaComponent implements OnInit {
     //     '2022-05-10': [{ test: 1, hej: 2 }, 2, 3],
     //   },
     // });
-    // setDoc()
+  }
+
+  getStay(): void {
+    // TODO: implement loader
+    const stayId = this.route.snapshot.params['stayId'];
+    console.log(stayId);
+    const db = collection(this.firestore, 'stays');
+    const docRef = doc(db, stayId);
+    const docSnap = getDoc(docRef);
+    docSnap.then((doc) => {
+      console.log(doc.data());
+      if (!doc.data()) {
+        this.router.navigate(['/404']);
+      }
+    });
   }
 
   ngOnInit() {
