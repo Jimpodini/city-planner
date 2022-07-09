0598d2e5ae8793238795d6a1d843981cf6ddb664
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-09 13:54:56 +0200

    Remove dead code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
-import {
-  Firestore,
-  collectionData,
-  collection,
-  setDoc,
-  doc,
-} from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,19 +7,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // item$: Observable<any[]>;
-
-  constructor(public authService: AuthService, firestore: Firestore) {
-    // const test = collection(firestore, 'stays');
-    // this.item$ = collectionData(test);
-    // this.item$.subscribe((value) => console.log(value));
-    // setDoc(doc(test, 'Hhgph1QjkxcwCIWNJiAu'), {
-    //   activitiesPerDate: {
-    //     '2022-05-10': [{ test: 1, hej: 2 }, 2, 3],
-    //   },
-    // });
-    // setDoc()
-  }
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {
     console.log(this.authService.authObject);
