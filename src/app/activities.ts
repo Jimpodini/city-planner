4481f39e5b20c5212a01401dd8f2f3a86001e4bc
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-18 21:16:54 +0200

    Add img long to all activities

--- src/app/activities.ts
+++ src/app/activities.ts
@@ -1,20 +1,25 @@
+// TODO introduce types
+
 export const ACTIVITIES = [
   {
     name: 'Vasaparken',
     category: 'Parks',
     img: 'vasaparken.jpg',
+    imgLong: 'vasaparken_long.png',
     googlePlaceId: 'ChIJhxFeG3udX0YRmSWF9cSLKeQ',
   },
   {
     name: 'Melanders',
     category: 'Restaurants',
     img: 'melanders.jpg',
+    imgLong: 'melanders_long.jpg',
     googlePlaceId: 'ChIJgaLDAmWdX0YR5nM3muriMC4',
   },
   {
     name: 'Boqueria',
     category: 'Bars',
     img: 'boqueria.jpg',
+    imgLong: 'boqueria_long.png',
     googlePlaceId: 'ChIJ5XkeUlydX0YRZI4qUQABRzU',
   },
 ];
