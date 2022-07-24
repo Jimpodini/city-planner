e2b9d2b57062f45c25c43e70c9035acc61dd455f
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-24 10:23:51 +0200

    Minor adjustment login component and set up routing to login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FeatureAreaComponent } from './core/feature-area/feature-area.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: '404',
     component: PageNotFoundComponent,
