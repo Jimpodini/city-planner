68d16693afa2dc77226ec28817d0d72a0a1d0757
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-26 11:24:25 +0200

    Setup login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private auth: Auth
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  submitForm() {
+    signInWithEmailAndPassword(
+      this.auth,
+      this.loginForm.controls.email.value,
+      this.loginForm.controls.password.value
+    );
+    console.log('Form submitted');
   }
-
 }
