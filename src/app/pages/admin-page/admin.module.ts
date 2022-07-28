013e2cb7e3a9ed7c8ea143e1c909e1a5b3c86d35
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-07-28 09:52:54 +0200

    Create location dialog for creating locations

--- src/app/pages/admin-page/admin.module.ts
+++ src/app/pages/admin-page/admin.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminPageComponent } from './admin-page.component';
-import { CommonModule } from '@angular/common';
 import { RegisterComponent } from 'src/app/components/register/register.component';
-import { LocationsComponent } from '../locations/locations.component';
+import {
+  CreateLocationDialog,
+  LocationsComponent,
+} from '../locations/locations.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +23,18 @@ import { LocationsComponent } from '../locations/locations.component';
     LoginComponent,
     RegisterComponent,
     LocationsComponent,
+    CreateLocationDialog,
   ],
-  imports: [CommonModule, AdminRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   providers: [],
 })
 export class AdminModule {}
