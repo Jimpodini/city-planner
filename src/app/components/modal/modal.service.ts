9ac7c4b3c2a64263395d76680be122d08dea1825
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-10-12 07:03:58 +0200

    Extend modal to also hold setsOfActivities

--- src/app/components/modal/modal.service.ts
+++ src/app/components/modal/modal.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 
+export type ModalType = 'activities' | 'setsOfActivities';
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
   showModal = false;
   date = '';
+  modalType!: ModalType;
 
   constructor() {}
 
-  openModal(date: string) {
+  openModal(date: string, modalType: ModalType = 'setsOfActivities') {
     this.showModal = true;
     this.date = date;
+    this.modalType = modalType;
   }
 }
