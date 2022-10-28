b31c2bda05d94a37c126de4f279de8b75135c4c8
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-10-28 08:02:10 +0200

    Password reset functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from '@angular/fire/auth';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +14,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  resetPasswordMode = false;
+
   loginForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   });
+
+  resetPasswordForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+
   constructor(
     private formBuilder: NonNullableFormBuilder,
     private auth: Auth,
-    private router: Router
+    private router: Router,
+    private snackbarService: SnackbarService
   ) {}
 
   ngOnInit(): void {}
@@ -29,6 +42,23 @@ export class LoginComponent implements OnInit {
     ).then(() => {
       this.router.navigate(['/admin']);
     });
-    console.log('Form submitted');
+  }
+
+  resetPassword() {
+    sendPasswordResetEmail(
+      this.auth,
+      this.resetPasswordForm.controls.email.value
+    )
+      .then(() => {
+        this.resetPasswordMode = false;
+        this.resetPasswordForm.controls.email.setValue('');
+        this.snackbarService.openSnackbar(
+          'success',
+          'A password reset email has been sent'
+        );
+      })
+      .catch(() =>
+        this.snackbarService.openSnackbar('error', 'Something went wrong')
+      );
   }
 }
